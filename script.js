6cc4292aac039c7481754d8ecb80de5cf3c0c5a6
AhmetAzizov <ahmet.azizov@gmail.com>
2023-01-30 04:41:10 +0100

    minor fix

--- script.js
+++ script.js
@@ -3,14 +3,21 @@
 
 var clicked = false;
 
-document.getElementsByClassName("main-container")[0].addEventListener("click", function(){
-    if(clicked == false){
-        document.getElementsByClassName("after")[0].style.opacity="0.6";
-        document.getElementsByClassName("image-context")[0].style.opacity="1";
-        clicked = true;
-    }else{
-        document.getElementsByClassName("after")[0].style.opacity="0";
-        document.getElementsByClassName("image-context")[0].style.opacity="0";
-        clicked = false; 
-    }
-});
+for(var i = 0; i < 11; i++){
+imageContainerListener(i);
+
+}
+
+function imageContainerListener(i){
+    document.getElementsByClassName("main-container")[i].addEventListener("click", function(){
+        if(clicked == false){
+            document.getElementsByClassName("after")[i].style.opacity="0.6";
+            document.getElementsByClassName("image-context")[i].style.opacity="1";
+            clicked = true;
+        }else{
+            document.getElementsByClassName("after")[i].style.opacity="0";
+            document.getElementsByClassName("image-context")[i].style.opacity="0";
+            clicked = false; 
+        }    });
+    
+}
