a0e8ce03d5b0d69cc5f8c10b127f4ff622f107a4
AhmetAzizov <ahmet.azizov@gmail.com>
2023-02-07 22:10:51 +0100

    Changed navigation bar layout

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,11 +1,11 @@
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-  if (!document.getElementsByClassName("nav-items")[0].classList.contains("nav-items-width")) {
+  if (!document.getElementsByClassName("nav-items")[0].classList.contains("nav-items-opened")) {
 
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementsByClassName("nav-bar")[0].style.top = "0";
+    document.getElementsByClassName("nav-bar")[0].style.top = "10px";
   } else {
     document.getElementsByClassName("nav-bar")[0].style.top = "-100px";
   }
@@ -22,5 +22,5 @@ var navButton = document.getElementsByClassName("menu-button-container")[0];
 var navItems = document.getElementsByClassName("nav-items")[0]
 
 navButton.addEventListener("click", function(){
-  navItems.classList.toggle("nav-items-width");
+  navItems.classList.toggle("nav-items-opened");
 });
