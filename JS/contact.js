c86a54d040ad8d2bb4b1355ff50f5dd598c7e499
AhmetAzizov <ahmet.azizov@gmail.com>
2023-02-16 19:44:31 +0100

    Updated submit form

--- JS/contact.js
+++ JS/contact.js
@@ -13,4 +13,19 @@ function copyIconButton(index) {
 }
 
 
+function sendEmail() {
+    var name = document.querySelector(".form-name").value;
+    var email = document.querySelector(".form-email").value;
+    var message = document.querySelector(".form-message").value;
 
+    Email.send({
+        SecureToken : "72ce78b0-f0ee-4d3c-b3a0-d41bc5e8fdf5",
+        To : 'ahmet.azizov@hotmail.com',
+        From : "ahmet.azizov@gmail.com",
+        Subject : "Mail sent by: " + email,
+        Body : "Name: " + name + "<br> Email: " + email + "<br> Message: " + message
+    }).then(
+    message => alert("Message Sent Successfully")
+    );
+
+}
