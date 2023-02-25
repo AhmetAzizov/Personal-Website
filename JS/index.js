348c7a7009db3ee3cc9c547f3e65fd8d0659bf60
AhmetAzizov <ahmet.azizov@gmail.com>
2023-02-25 19:30:35 +0100

    style changes

--- JS/index.js
+++ JS/index.js
@@ -7,3 +7,21 @@ const backgroundImage = document.querySelector(".background-image");
 backgroundImage.src = `wallpapers/${randomNumber(0,13)}.jpg`;
 
 
+
+
+
+
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
