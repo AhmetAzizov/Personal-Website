b17d2fe6eb0cbb516163dd91c7a6dcb91678c16b
AhmetAzizov <ahmet.azizov@gmail.com>
2023-02-26 00:07:16 +0100

    Changed style in contact page

--- JS/contact.js
+++ JS/contact.js
@@ -29,3 +29,16 @@ function sendEmail() {
     );
 
 }
+
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
