678feda095ca40c5c2dc760df722f123a9d087a1
AhmetAzizov <ahmet.azizov@gmail.com>
2023-03-07 23:56:25 +0100

    changed style of projects page

--- JS/navbar.js
+++ JS/navbar.js
@@ -4,33 +4,39 @@ var navBar = document.getElementsByClassName("nav-bar")[0];
 
 var prevScrollpos = wrapper.scrollTop;
 
-wrapper.onscroll = function() {
-  if (!document.getElementsByClassName("nav-items")[0].classList.contains("nav-items-opened")) {
+wrapper.onscroll = function () {
+  if (
+    !document
+      .getElementsByClassName("nav-items")[0]
+      .classList.contains("nav-items-opened")
+  ) {
+    var currentScrollPos = wrapper.scrollTop;
+    if (prevScrollpos > currentScrollPos) {
+      navBar.classList.remove("nav-bar-closed");
+    } else {
+      navBar.classList.add("nav-bar-closed");
+    }
 
-  var currentScrollPos = wrapper.scrollTop;
-  if (prevScrollpos > currentScrollPos) {
-    navBar.classList.remove("nav-bar-closed");
-  } else {
-    navBar.classList.add("nav-bar-closed");
+    if (currentScrollPos <= 0) {
+      navBar.classList.remove("nav-bar-closed");
+    }
+
+    prevScrollpos = currentScrollPos;
   }
-
-  if (currentScrollPos <= 0) {
-    navBar.classList.remove("nav-bar-closed");
-  }
-
-  prevScrollpos = currentScrollPos;
-}
-}
+};
 // ------------------------------------------------------------------------------------------
 
-document.getElementsByClassName("menu-button-container")[0].addEventListener("click", function(){
-  document.getElementsByClassName("menu-button-container")[0].classList.toggle("change");
-});
-
+document
+  .getElementsByClassName("menu-button-container")[0]
+  .addEventListener("click", function () {
+    document
+      .getElementsByClassName("menu-button-container")[0]
+      .classList.toggle("change");
+  });
 
 var navButton = document.getElementsByClassName("menu-button-container")[0];
-var navItems = document.getElementsByClassName("nav-items")[0]
+var navItems = document.getElementsByClassName("nav-items")[0];
 
-navButton.addEventListener("click", function(){
+navButton.addEventListener("click", function () {
   navItems.classList.toggle("nav-items-opened");
 });
